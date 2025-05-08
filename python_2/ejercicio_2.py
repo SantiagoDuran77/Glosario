16513f9ee2d01f823f665a28beaac95469978ea4
SantiagoDuran77 <santiagoduranr7@gmail.com>
2025-05-08 17:05:57 -0500

    Eliminar carpeta python_2

--- python_2/ejercicio_2.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio 2: Calcular el interés simple por meses
-capital = float(input("Ingrese el capital: "))
-tasa = float(input("Ingrese la tasa de interés anual (%): "))
-tiempo = float(input("Ingrese el tiempo (meses): "))
-interes = (capital * (tasa / 12) * tiempo) / 100
-print(f"El interés simple es: {interes}")
